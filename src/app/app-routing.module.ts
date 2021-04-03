a664b51c9dc937ccdb67d2e1e487929bbe817407
Marco Romanutti <m.romanutti@gmail.com>
2021-04-03 11:28:49 +0200

    Fix resolver bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,29 +10,19 @@ import { RecipesResolverService } from './recipes/recipes-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [RecipesResolverService]
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipesResolverService]
-      }
-    ]
-  },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: '', component: RecipeStartComponent },
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+    { path: ':id/edit', component: RecipeEditComponent,  resolve: [RecipesResolverService] },
+  ] },
+  { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+
+}
